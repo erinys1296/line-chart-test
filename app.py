6a911651ed3171af2ecb4273a4099a07ce59e602
erinys1296 <erinys1296@gmail.com>
2023-10-01 21:19:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-
+PLAYING = False
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
@@ -26,11 +26,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.message.text)
-    if event.message.text == '開始':
+    
+    if event.message.text == '開始' and PLAYING != False:
         answer = random.randint(1,100)
         message = TextSendMessage(text="請從1到100中猜個數字 " + str(answer))
         line_bot_api.reply_message(event.reply_token, message)
+    elif PLAYING == True:
+        try:
+            guass = int(event.message.text)
+            if guass == answer:
+                UserName = event.source.display_name
+                username = line_bot_api.get_profile(UserName)
+                line_bot_api.reply_message(event.reply_token, '恭喜' + username + '猜對啦!')
+                PLAYING = False
+            else:
+                line_bot_api.reply_message(event.reply_token, '猜錯囉!請繼續!')
+        except:
+            line_bot_api.reply_message(event.reply_token, '請輸入數字')
+        
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
